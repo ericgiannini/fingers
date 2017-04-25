8771e6fc07e765944db550f020e86ceddf8bd46e
ericgiannini <eric.giannini@yahoo.com>
2017-04-25 14:11:46 -0700

    Added circle for FaceView

--- fingers/fingers/FaceView.swift
+++ fingers/fingers/FaceView.swift
@@ -9,14 +9,23 @@
 import UIKit
 
 class FaceView: UIView {
-    
-    
-
 
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
         // Drawing code
+        
+        let faceRadius = min(bounds.size.width, bounds.size.height) / 2
+        let faceCenter = CGPoint(x: bounds.midX, y: bounds.midY)
+        let path = UIBezierPath(arcCenter: faceCenter, radius: faceRadius, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: false)
+        
+        path.lineWidth = 5.0
+        
+        UIColor.blue.set()
+        
+        path.stroke()
+        
+        
     }
 
 
--- fingers/fingers/ViewController.swift
+++ fingers/fingers/ViewController.swift
@@ -9,13 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+//        view.addSubview(faceView)
+        
     }
 
     override func didReceiveMemoryWarning() {
