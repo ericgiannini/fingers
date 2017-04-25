b8f7e0af22a8cf4afec31de8accd5d5ee398f54f
ericgiannini <eric.giannini@yahoo.com>
2017-04-25 13:57:20 -0700

    Updating project's base

---
+++ fingers/fingers/FaceView.swift
@@ -0,0 +1,23 @@
+//
+//  FaceView.swift
+//  fingers
+//
+//  Created by Eric Giannini on 4/25/17.
+//  Copyright © 2017 Unicorn Mobile, LLC. All rights reserved.
+//
+
+import UIKit
+
+class FaceView: UIView {
+    
+    
+
+
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+
+
+}
