fb8a514f9c825e96641c36f16b32604f540d44a2
ericgiannini <eric.giannini@yahoo.com>
2017-04-25 15:10:12 -0700

    Updated project

--- fingers/fingers/FaceView.swift
+++ fingers/fingers/FaceView.swift
@@ -8,11 +8,28 @@
 
 import UIKit
 
+/* 
+
+ @IBDesignable provides Interface Builder access to the explicitly typed @IBInspectable properties
+ 
+*/
+
+@IBDesignable
 class FaceView: UIView {
     
+    @IBInspectable
     var scale: CGFloat = 0.9
     
+    @IBInspectable
     var eyesOpen: Bool = true
+    
+    @IBInspectable
+    var lineWidth: CGFloat = 5.0
+    
+    @IBInspectable
+    var color: UIColor = UIColor.blue
+    
+    public var mouthCurvature: Double = 1.0 // is full smile; -1.0 is full frown
     
     private var faceRadius: CGFloat {
         
@@ -30,7 +47,7 @@ class FaceView: UIView {
         
         let path = UIBezierPath(arcCenter: faceCenter, radius: faceRadius, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: false)
         
-        path.lineWidth = 5.0
+        path.lineWidth = lineWidth
         
         return path
         
@@ -46,13 +63,26 @@ class FaceView: UIView {
         let mouthWidth = faceRadius / Ratios.faceRadiusToMouthWidth
         let mouthHeight = faceRadius / Ratios.faceRadiusToMouthHeight
         let mouthOffset = faceRadius / Ratios.faceRadiusToMouthOffset
-       
+        
         let mouthRect = CGRect(x: faceCenter.x - mouthWidth / 2,
                                y: faceCenter.y + mouthOffset,
                                width: mouthWidth,
                                height: mouthHeight)
         
-        let path = UIBezierPath(rect: mouthRect)
+        let smileOffset = CGFloat(max(-1, min(mouthCurvature, 1))) * mouthRect.height
+        
+        let start = CGPoint(x: mouthRect.minX, y: mouthRect.midY)
+        let end = CGPoint(x: mouthRect.maxX, y: mouthRect.midY)
+        
+        let cp1 = CGPoint(x: start.x + mouthRect.width / 3, y: start.y + smileOffset)
+        let cp2 = CGPoint(x: end.x - mouthRect.width / 3, y: start.y + smileOffset)
+        
+        let path = UIBezierPath()
+        path.move(to: start)
+        path.addCurve(to: end, controlPoint1: cp1, controlPoint2: cp2)
+        
+        
+        path.lineWidth = lineWidth
         
         return path
     }
@@ -71,9 +101,9 @@ class FaceView: UIView {
         let eyeCenter = centerOfEye(eye)
         
         let path: UIBezierPath
-
+        
         if eyesOpen {
-
+            
             path = UIBezierPath(arcCenter: eyeCenter, radius: eyeRadius, startAngle: 0, endAngle: CGFloat.pi * 2, clockwise: true)
             
             
@@ -86,8 +116,8 @@ class FaceView: UIView {
             
         }
         
-
-        path.lineWidth = 5.0
+        
+        path.lineWidth = lineWidth
         
         return path
         
@@ -99,11 +129,9 @@ class FaceView: UIView {
     override func draw(_ rect: CGRect) {
         // Drawing code
         
-        
-        UIColor.blue.set()
+        color.set()
         
         [pathForFace(), pathForEye(.left), pathForEye(.right),  pathForMouth()].forEach {$0.stroke()}
-
         
     }
     
